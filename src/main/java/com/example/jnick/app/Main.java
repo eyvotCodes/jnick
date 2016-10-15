a503425362537929e4a0d7222f51f3b3e976965d
Fabián <Fabian_Boss@Outlook.com>
2016-10-15 14:08:46 -0500

    generator completed, documented and tested

--- src/main/java/com/example/jnick/app/Main.java
+++ src/main/java/com/example/jnick/app/Main.java
@@ -1,31 +1,17 @@
 package com.example.jnick.app;
 
-import com.example.jnick.generator.Node;
-
-import java.util.ArrayList;
-
 /**
- * @author fabian 
+ * @author fabian
  */
 public class Main {
-    
+
     /**
      * @param args the command line arguments
      */
     public static void main(String args[]) {
-    
+
         System.out.println("working in generator...");
 
-        ArrayList<Node> children = new ArrayList<Node>();
-        children.add(new Node('a'));
-        children.add(new Node('b'));
-        children.add(new Node('c'));
-
-        Node x = new Node('x');
-        x.setChildren(children);
-        
-        System.out.println(x);
-    
     }
-    
+
 }
