0e606a40caf346025fbd639154631d1397f207a8
Fabián <Fabian_Boss@Outlook.com>
2016-10-15 18:05:22 -0500

    single vocal discriminator

--- src/main/java/com/example/jnick/discriminator/Discriminator.java
+++ src/main/java/com/example/jnick/discriminator/Discriminator.java
@@ -5,12 +5,38 @@ package com.example.jnick.discriminator;
  */
 public abstract class Discriminator {
 
-    protected final String CHARACTER_SEPARATOR = "-";
-    protected final String CHARACTER_BLANK     = "";
+    protected final int         NUMBER_OF_POSITIONS =   3;
+    protected final boolean[]   POSITION_NOT_FOUND_ERROR =
+                                    new boolean[] {false, false, false};
+
+    protected final String      CHARACTER_SEPARATOR = "-";
+    protected final String      CHARACTER_BLANK     =  "";
 
     public Discriminator() {}
 
-    public abstract boolean     isValid(String token);
-    public abstract boolean[]   getValidPositions(String token);
+
+    /**
+     * Check if a string is valid as token.
+     *
+     * @param   token single, double or triple character string.
+     * @return        valid or not
+     * */
+    public abstract boolean isValid(String characters);
+
+    /**
+     * Obtains the valid positions that a token can be have into a nickname.
+     *
+     * @param   token valid token
+     * @return        positions
+     * */
+    public abstract boolean[] getValidPositions(String token);
+
+    /**
+     * Obtains the number of single, double or triple character tokens that
+     * can be valid.
+     *
+     * @return number of valid tokens in a class.
+     * */
+    public abstract int getCollectionSize();
 
 }
---
+++ src/main/java/com/example/jnick/discriminator/SingleVocalDiscriminator.java
@@ -0,0 +1,69 @@
+package com.example.jnick.discriminator;
+
+/**
+ * @author fabian
+ */
+public class SingleVocalDiscriminator extends Discriminator {
+
+    private final int           A =  0, E =  1, I =  2, O =  3, U =  4;
+    private       boolean[][]   positionsMatrix;
+
+    private final String        SINGLE_VOCALS = "a-e-i-o-u";
+
+
+    public SingleVocalDiscriminator() {
+        initPositionsMatrix();
+    }
+
+
+    /**
+     * {@inheritDoc}
+     * */
+    @Override public boolean isValid(String singleCharacter) {
+        for(String singleVocal:SINGLE_VOCALS
+                .split(CHARACTER_SEPARATOR)) {
+            if(singleCharacter.equals(singleVocal)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * {@inheritDoc}
+     * */
+    @Override public boolean[] getValidPositions(String singleVocal) {
+        switch(singleVocal) {
+            case "a": return positionsMatrix[A];
+            case "e": return positionsMatrix[E];
+            case "i": return positionsMatrix[I];
+            case "o": return positionsMatrix[O];
+            case "u": return positionsMatrix[U];
+        }
+        return POSITION_NOT_FOUND_ERROR;
+    }
+
+    /**
+     * {@inheritDoc}
+     * */
+    @Override public int getCollectionSize() {
+        return SINGLE_VOCALS
+                .split(CHARACTER_SEPARATOR)
+                .length;
+    }
+
+    /**
+     * Initializes the matrix that contains all positions about
+     * single vocal tokens.
+     * */
+    private void initPositionsMatrix() {
+        this.positionsMatrix =
+            new boolean[getCollectionSize()][NUMBER_OF_POSITIONS];
+        positionsMatrix[A] = new boolean[] {true,true,true};
+        positionsMatrix[E] = new boolean[] {true,true,true};
+        positionsMatrix[I] = new boolean[] {true,true,true};
+        positionsMatrix[O] = new boolean[] {true,true,true};
+        positionsMatrix[U] = new boolean[] {true,true,true};
+    }
+
+}
