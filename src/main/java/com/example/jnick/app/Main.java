c05acb224bbcab36b3c1c1a0a8a2153dd9a0ccad
Fabián <Fabian_Boss@Outlook.com>
2016-10-15 14:50:17 -0500

    discriminator initial commit

--- src/main/java/com/example/jnick/app/Main.java
+++ src/main/java/com/example/jnick/app/Main.java
@@ -10,7 +10,7 @@ public class Main {
      */
     public static void main(String args[]) {
 
-        System.out.println("working in generator...");
+        System.out.println("working in discriminator...");
 
     }
 
